4de30cb4d359fa36ec3975402129fade828dad31
mdpunch <punch@in2p3.fr>
2019-11-27 18:48:24 +0100

    Updqte propgalspiflash to recognize Macronix MX25L32
    We are using the new flash memory Macronix MX25L32, and found that the above change allows it to be recognized.
    Would be nice to have this in the package, thanks.

--- progalgspiflash.cpp
+++ progalgspiflash.cpp
@@ -370,7 +370,10 @@ int ProgAlgSPIFlash::spi_flashinfo_m25p_mx25l(unsigned char *buf, int is_mx25l)
                 fbuf[1], fbuf[2]);
         switch (fbuf[2])
           {
-          case 0x17:
+          case 0x16: // Adding support for new flash Macronix MX25L32
+            pages = 16384;
+            break;
+	  case 0x17:
             pages = 262144;
             sector_size = 65536;
             break;
